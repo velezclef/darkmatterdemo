b945493c07996fb8b005f2de5a49d9e9c5f19728
velezclef <maurovelez85@gmail.com>
2021-05-14 10:51:30 -0500

    Added ktx-game and ktx-log dependencies. Created initial screens.

--- core/src/main/kotlin/com/example/darkmatter/DarkMatter.kt
+++ core/src/main/kotlin/com/example/darkmatter/DarkMatter.kt
@@ -1,11 +1,23 @@
 package com.example.darkmatter
 
-import com.badlogic.gdx.Game
-import com.example.darkmatter.FirstScreen
+import com.badlogic.gdx.Application.LOG_DEBUG
+import com.badlogic.gdx.Gdx
+import com.example.darkmatter.screen.FirstScreen
+import com.example.darkmatter.screen.SecondScreen
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
+import ktx.log.Logger
+import ktx.log.debug
+import ktx.log.logger
 
-/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
-class DarkMatter : Game() {
+private val LOG: Logger = logger<DarkMatter>()
+
+class DarkMatter : KtxGame<KtxScreen>() {
     override fun create() {
-        setScreen(FirstScreen())
+        LOG.debug { "Create game instance" }
+        Gdx.app.logLevel = LOG_DEBUG
+        addScreen(FirstScreen(this))
+        addScreen(SecondScreen(this))
+        setScreen<FirstScreen>()
     }
 }
---
+++ core/src/main/kotlin/com/example/darkmatter/screen/DarkMatterScreen.kt
@@ -0,0 +1,6 @@
+package com.example.darkmatter.screen
+
+import com.example.darkmatter.DarkMatter
+import ktx.app.KtxScreen
+
+abstract class DarkMatterScreen(val game: DarkMatter) : KtxScreen
